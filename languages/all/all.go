fda593d034114cec6be68ec07d1ee5c4313122fc
Denys Smirnov <denys@sourced.tech>
2019-11-21 18:11:51 +0200

    enable typescript by default
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- languages/all/all.go
+++ languages/all/all.go
@@ -4,4 +4,5 @@ import (
 	_ "github.com/src-d/imports/languages/golang"
 	_ "github.com/src-d/imports/languages/java"
 	_ "github.com/src-d/imports/languages/javascript"
+	_ "github.com/src-d/imports/languages/typescript"
 )
